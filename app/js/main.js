c919f7df9836a97e65ecb9d8d6647259d0ce72a3
evgKireev <bob1_k@mail.ru>
2022-11-19 15:33:44 +0300

    form

--- app/js/main.js
+++ app/js/main.js
@@ -1,33 +1,35 @@
-const btnHeader = document.querySelector('.header-info__order-btn');
-const btnVideo = document.querySelector('.video__btn');
-const btnAuthor = document.querySelector('.author__btn');
-const btnGallery = document.querySelector('.gallery__btn');
-const modal = document.querySelector('.modal');
-const modalOk = document.querySelector('.modal-ok');
-const modalBtn = document.querySelector('.modal__form-btn');
-const modalTitle = document.querySelector('.modal__form-title');
-const modalCloset = document.querySelector('.modal__form-closet');
+ const btnHeader = document.querySelector('.header-info__order-btn');
+ const btnVideo = document.querySelector('.video__btn');
+ const btnAuthor = document.querySelector('.author__btn');
+ const btnGallery = document.querySelector('.gallery__btn');
+ const modal = document.querySelector('.modal');
+ const modalOk = document.querySelector('.modal-ok');
+ const modalBtn = document.querySelector('.modal__form-btn');
+ const modalTitle = document.querySelector('.modal__form-title');
+ const modalCloset = document.querySelector('.modal__form-closet');
 
-$(document).ready(function () {
-  $('form').submit(function () {
-    var th = $(this);
-    $.ajax({
-      type: 'POST',
-      url: 'mail.php', 
-      data: th.serialize(),
-    }).done(function () {
-      modal.classList.remove('showModal');
-      modalOk.classList.add('showModal');
-      setTimeout(() => {
-        modalOk.classList.remove('showModal');
-      }, 3000);
-      setTimeout(function () {
-        th.trigger('reset');
-      }, 1000);
-    });
-    return false;
-  });
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
 });
+
 
 function render() {
   // modalBtn.addEventListener('click', (e) => {
