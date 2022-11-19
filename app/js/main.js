49981bd406fb2f0e7114f2aa5c67479d2678f814
evgKireev <bob1_k@mail.ru>
2022-11-19 15:27:17 +0300

    form

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,43 @@
-function render() {
-  const btnHeader = document.querySelector('.header-info__order-btn');
-  const btnVideo = document.querySelector('.video__btn');
-  const btnAuthor = document.querySelector('.author__btn');
-  const btnGallery = document.querySelector('.gallery__btn');
-  const modal = document.querySelector('.modal');
-  const modalOk = document.querySelector('.modal-ok');
-  const modalBtn = document.querySelector('.modal__form-btn');
-  const modalTitle = document.querySelector('.modal__form-title');
-  const modalCloset = document.querySelector('.modal__form-closet');
+const btnHeader = document.querySelector('.header-info__order-btn');
+const btnVideo = document.querySelector('.video__btn');
+const btnAuthor = document.querySelector('.author__btn');
+const btnGallery = document.querySelector('.gallery__btn');
+const modal = document.querySelector('.modal');
+const modalOk = document.querySelector('.modal-ok');
+const modalBtn = document.querySelector('.modal__form-btn');
+const modalTitle = document.querySelector('.modal__form-title');
+const modalCloset = document.querySelector('.modal__form-closet');
 
-  modalBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal.classList.remove('showModal');
-    modalOk.classList.add('showModal');
-    setTimeout(() => {
-      modalOk.classList.remove('showModal');
-    }, 3000);
+$(document).ready(function () {
+  $('form').submit(function () {
+    var th = $(this);
+    $.ajax({
+      type: 'POST',
+      url: 'mail.php', 
+      data: th.serialize(),
+    }).done(function () {
+      modal.classList.remove('showModal');
+      modalOk.classList.add('showModal');
+      setTimeout(() => {
+        modalOk.classList.remove('showModal');
+      }, 3000);
+      setTimeout(function () {
+        th.trigger('reset');
+      }, 1000);
+    });
+    return false;
   });
+});
+
+function render() {
+  // modalBtn.addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   modal.classList.remove('showModal');
+  //   modalOk.classList.add('showModal');
+  //   setTimeout(() => {
+  //     modalOk.classList.remove('showModal');
+  //   }, 3000);
+  // });
 
   modalCloset.addEventListener('click', () => {
     modal.classList.remove('showModal');
